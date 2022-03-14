5133bf6b440134c35dd4d6a8cf7347b5a42a3e7b
cjw20 <cjw20@iu.edu>
2022-03-14 16:31:45 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] GameObject playerBox;
+    Transform boxTransform;
+
+    public bool rotating;
+    public float rotateSpeed;
+    public float duration;
+
+    public Quaternion target;
+    public Vector3 oldRotation;
+    public Vector3 targetAngle;
+    float t;
+
+    void Awake()
+    {
+        boxTransform = playerBox.GetComponent<Transform>();
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotating)
+        {
+            t += (Time.deltaTime * rotateSpeed); 
+            //boxTransform.rotation = Quaternion.RotateTowards(boxTransform.rotation, target, 3);
+            boxTransform.rotation = Quaternion.Lerp(boxTransform.rotation, target, t);
+            
+            if(t >= 1   )
+            {
+                rotating = false;
+                t = 0;
+            }    
+        }
+    }
+
+    public void RotateRight()
+    {
+        if (rotating)
+            return;
+        oldRotation = boxTransform.rotation.eulerAngles;
+        targetAngle = oldRotation + new Vector3(0, 0, 90);
+        target = Quaternion.Euler(targetAngle);
+        rotating = true;
+        
+    }
+    
+    public void RotateLeft()
+    {
+        if (rotating)
+            return;
+        oldRotation = boxTransform.rotation.eulerAngles;
+        targetAngle = oldRotation + new Vector3(0, 0, -90);
+        target = Quaternion.Euler(targetAngle);
+        rotating = true;
+        
+    }
+
+    
+}
