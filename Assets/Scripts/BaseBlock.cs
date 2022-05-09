e77a5e1b3747850ba2d2b3009d38c1c14650cc33
cjw20 <cjw20@iu.edu>
2022-05-09 14:05:26 -0400

    Added scaling functions

--- Assets/Scripts/BaseBlock.cs
+++ Assets/Scripts/BaseBlock.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class BaseBlock : MonoBehaviour
 {
     public BlockColor color;
+    [SerializeField] Transform pivotTransform; //transform of pivot point of block. Scale this to adjust size of block
+
+    public void AddSize(Vector2 amount)
+    {
+        pivotTransform.localScale += new Vector3(amount.x, amount.y, 0f);
+    }
+
+    public void ResetSize()
+    {
+        pivotTransform.localScale = new Vector3(1f, 1f, 1f);
+        //update score
+    }
 }
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public enum BlockColor { red, blue, green, yellow, white };
 public class Block : MonoBehaviour
-{
-    public bool baseBlock;
+{    
     public BlockColor blockColor;
-
 
     private void Start()
     {
@@ -16,19 +14,44 @@ public class Block : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        BlockColor collisionColor = collision.gameObject.GetComponent<Block>().blockColor;
+        BaseBlock contactBase = collision.gameObject.GetComponent<BaseBlock>();
+        
         if(blockColor == BlockColor.white)
         {
-            //clear
+            contactBase.ResetSize();
         }
-        else if(collisionColor == blockColor)
+        else if(contactBase.color == blockColor)
         {
-           //attach
+            contactBase.AddSize(CalculateScale());
         }      
 
         else
         {
             //lose life
         }
+
+        Destroy(this); //removes block after collision 
+    }
+
+    Vector2 CalculateScale()
+    {
+        Vector2 amountToAdd = new Vector2(0, 0);
+        switch (blockColor)
+        {
+            case BlockColor.red:
+                amountToAdd += new Vector2(1, 0);
+                break;
+            case BlockColor.blue:
+                amountToAdd += new Vector2(1, 0);
+                break;
+            case BlockColor.yellow:
+                amountToAdd += new Vector2(0, 1);
+                break;
+            case BlockColor.green:
+                amountToAdd += new Vector2(0, 1);
+                break;
+        }
+        
+        return amountToAdd;
     }
 }
